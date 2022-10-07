880abf1079f215300bbda76a44e518fccaa084be
Taichi Eto <38776373+TaichiEto@users.noreply.github.com>
2022-10-07 11:40:59 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+for i in range(1, 500):
+    if i % 15 == 0:
+        print("Fizz Buzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+
